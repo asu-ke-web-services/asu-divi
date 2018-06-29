f5f9db54021c57c23b95644190a1ac829e735280
Wayne Parkhurst <wsparkhu@asu.edu>
2018-06-28 17:39:44 -0700

    v1.0.17 Fix for home icon & content font weight

--- functions.php
+++ functions.php
@@ -2,19 +2,19 @@
 
 /* Register, enqueue scripts, execute action */
 function asuwp_enqueue_scripts() {
-    
+
     wp_register_style( 'divi', get_template_directory_uri() . '/style.css');
     wp_register_style( 'asu-divi', get_stylesheet_directory_uri().'/style.css', array('divi'), wp_get_theme()->get('Version'));
     wp_register_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', false, '4.7.0' );
     wp_register_style( 'roboto-font', 'https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i');
-    
+
     // wp_register_script( 'asu-header', 'https://www.asu.edu/asuthemes/4.6/heads/default.shtml', array() , '4.6', false );
-    
+
     wp_enqueue_style( 'divi' );
     wp_enqueue_style( 'asu-divi' );
     wp_enqueue_style( 'font-awesome' );
     wp_enqueue_style( 'roboto-font' );
-    
+
     // wp_enqueue_script( 'asu-header' );
 
 }
@@ -44,7 +44,7 @@ function asuwp_load_global_footer() {
 function asuwp_add_home_menu_icon ( $items, $args ) {
     if ($args->theme_location == 'primary-menu') {
 
-        if (is_home()) {
+        if (is_front_page()) {
             $homeicon = '<li id="menu-item-home" class="menu_item current-menu-item">';
         } else {
             $homeicon = '<li id="menu-item-home" class="menu_item">';
@@ -54,7 +54,7 @@ function asuwp_add_home_menu_icon ( $items, $args ) {
         $homeicon .= '<span class="fa fa-home" aria-hidden="true"></span>';
         $homeicon .= '</a>';
         $homeicon .= '</li>';
-        
+
         $items = $homeicon . $items;
     }
     return $items;
